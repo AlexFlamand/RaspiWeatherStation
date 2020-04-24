e89d511ccdd46ec44bf36106687bc016e09c2603
Alex Flamand <pafword@gmail.com>
2020-04-23 21:16:01 -0400

    Added an additional script that changes the temperature units and what is written to the console.

--- Source Code/master_test.py
+++ Source Code/master_test.py
@@ -12,21 +12,21 @@ i2c = busio.I2C(board.SCL, board.SDA)
 dps310 = adafruit_dps310.DPS310(i2c)
 dhtDevice = adafruit_dht.DHT22(board.D4)
 
-def sensor_reader(temperature_c, temperature_f, humidity):
+def sensor_reader():
     
     while True:
 
         # Print the values to the console.
         try:
             print("Pressure = %.2f hPa"%dps310.pressure)
+            time.sleep(1)
             temperature_c = dhtDevice.temperature
             temperature_f = temperature_c * (9 / 5) + 32
             humidity = dhtDevice.humidity
+            print("Temp: {:.1f} F / {:.1f} C \nHumidity: {}% "
+                .format(temperature_f, temperature_c, humidity))
+            print("")
             return [float(temperature_c), float(temperature_f), float(humidity)]
-            
-#            print("Temp: {:.1f} F / {:.1f} C \nHumidity: {}% "
-#                .format(temperature_f, temperature_c, humidity))
-#            print("")
             
         # Errors happen fairly often with DHT sensors, and will occasionally throw exceptions.
         except RuntimeError as error:
@@ -36,42 +36,31 @@ def sensor_reader(temperature_c, temperature_f, humidity):
         # Waits 60 seconds before repeating.
         time.sleep(60)
 
+threading.Thread(target=sensor_reader, name="Thread #1", daemon=True).start()
 
-class csvWriter:
+# Fetches the date and time for future file naming and data logging operations.
+starttime=time.time()
+now = datetime.now()
+
+# Writes the header for the .csv file once.
+with open('Weather Log %s.csv' % now, 'w', newline='') as f:
+    fieldnames = ['Time', 'Temperature (C)', 'Humidity (%)', 'Pressure (hPa)']
+    thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+    thewriter.writeheader()
+
+# Fetches the date and time.
+while True:
     
-    threading.Thread(target=sensor_reader, name="Thread #1", daemon=True).start()
-
-    # Fetches the date and time for future file naming and data logging operations.
-    starttime=time.time()
-    now = datetime.now()
-
-    with open('Weather Log %s.csv' % now, 'w', newline='') as f:
-        fieldnames = ['Time', 'Temperature (F)', 'Humidity (%)', 'Pressure (hPa)']
+    temperature_c_log = dhtDevice.temperature
+#    temperature_f_log = temperature_c_log * (9 / 5) + 32
+    humidity_log = dhtDevice.humidity
+    
+    # Writes incoming data to the .csv file.
+    with open('Weather Log %s.csv' % now, 'a', newline='') as f: 
+        fieldnames = ['TIME', 'TEMP', 'HUMI', 'PRES'] 
         thewriter = csv.DictWriter(f, fieldnames=fieldnames)
-        thewriter.writeheader()
-            
-    # Writes the header for the .csv file once.
+        thewriter.writerow({'TIME' : now.strftime("%H:%M:%S"), 'TEMP' : temperature_c_log, 'HUMI' : humidity_log, 'PRES' : dps310.pressure})
 
-    # Fetches the date and time.
-    while True:
-        current_time = now.strftime("%H:%M:%S")
-
-        # When the below lines are uncommented, the console simulates data output from the sensors. Used to test csvWriter class.
-        # temp = random.randint(0,125)       
-        # humi = random.randint(0,100)       
-        # pres = random.randint(900,1000)    
-        
-        temperature_c_log = dhtDevice.temperature
-        temperature_f_log = temperature_c_log * (9 / 5) + 32
-        humidity_log = dhtDevice.humidity
-        
-        # Writes incoming data to the .csv file.
-        with open('Weather Log %s.csv' % now, 'a', newline='') as f: 
-            fieldnames = ['TIME', 'TEMP', 'HUMI', 'PRES'] 
-            thewriter = csv.DictWriter(f, fieldnames=fieldnames)
-            thewriter.writerow({'TIME' : current_time, 'TEMP' : temperature_f_log, 'HUMI' : humidity_log, 'PRES' : dps310.pressure})
-
-        # Writes a message confirming the data's entry into the log, then sets a 60 second repeat cycle.
-        print("New entry added.")
-        time.sleep(60.0 - ((time.time() - starttime) % 60.0)) # Repeat every ten seconds.
-
+    # Writes a message confirming the data's entry into the log, then sets a 60 second repeat cycle.
+    print("New entry added.")
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0)) # Repeat every ten seconds.
